f3470f56a00a18610536e7fab459d079edf15d21
nianyuguai <840311035@qq.com>
2015-05-27 23:38:53 +0800

    20150527

--- com/nianyu/utils/db.py
+++ com/nianyu/utils/db.py
@@ -3,4 +3,145 @@ __author__ = 'lixiaojian'
 
 import MySQLdb
 
-class PyMysql():
+class DB:
+
+    error_code = ''  #MySQL错误号码
+    _instance = None  #本类的实例
+    _conn = None  #数据库conn
+    _cur = None  #游标
+    _TIMEOUT = 30  #默认超时30秒
+    _timecount = 0
+
+    def __init__(self, db_conf):
+        """
+        初始化连接，获取连接对象
+        """
+        try:
+            self._conn = MySQLdb.connect(
+                         host=db_conf['host'],
+                         port=db_conf['port'],
+                         user=db_conf['user'],
+                         passwd=db_conf['passwd'],
+                         db=db_conf['db'],
+                         charset=db_conf['charset'])
+        except MySQLdb.Error, e:
+            self.error_code = e.args[0]
+            error_msg = 'MySQL error : ', e.args[0], e.args[1]
+            print error_msg
+
+            # 如果没有超过预设超时时间，则再次尝试连接，
+            if self._timecount < self._TIMEOUT:
+                interval = 5
+                self._timecount += interval
+                time.sleep(interval)
+                return self.__init__(db_conf)
+            else:
+                raise Exception(error_msg)
+        self._cur = self._conn.cursor()
+        self._instance = MySQLdb
+
+    def query(self,sql):
+        """
+        select sql
+        """
+        try:
+            self._cur.execute("SET NAMES utf8")
+            result = self._cur.execute(sql)
+        except MySQLdb.Error, e:
+            self.error_code = e.args[0]
+            print "MySQL error : ", e.args[0], e.args[1]
+            result = False
+        return result
+
+    def update(self,sql):
+        """
+        update sql
+        """
+        try:
+            self._cur.execute("SET NAMES utf8")
+            result = self._cur.execute(sql)
+            self._conn.commit()
+        except MySQLdb.Error, e:
+            self.error_code = e.args[0]
+            print "数据库错误代码:",e.args[0],e.args[1]
+            result = False
+        return result
+
+    def insert(self,sql):
+        """
+        :return:执行 INSERT 语句。如主键为自增长int，则返回新生成的ID
+        """
+        try:
+            self._cur.execute("SET NAMES utf8")
+            self._cur.execute(sql)
+            self._conn.commit()
+            return self._conn.insert_id()
+        except MySQLdb.Error, e:
+            self.error_code = e.args[0]
+            return False
+
+    def fetchAllRows(self):
+        """
+        :return:返回结果列表
+        """
+        return self._cur.fetchall()
+
+    def fetchOneRow(self):
+        """
+        :return:返回一行结果，然后游标指向下一行。到达最后一行以后，返回None
+        """
+        return self._cur.fetchone()
+
+    def getRowCount(self):
+        """
+        :return:获取结果行数
+        """
+        return self._cur.rowcount
+
+    def commit(self):
+        """
+        数据库commit操作
+        """
+        self._conn.commit()
+
+    def rollback(self):
+        """
+        数据库回滚操作
+        """
+        self._conn.rollback()
+
+    def __del__(self):
+        """
+        释放资源（系统GC自动调用）
+        """
+        try:
+            self._cur.close()
+            self._conn.close()
+        except:
+            pass
+
+    def  close(self):
+        """
+        关闭数据库连接
+        """
+        self.__del__()
+
+
+
+if __name__ == '__main__':
+    db_conf = {
+        'host':'localhost',
+        'port': 3306,
+        'user':'root',
+        'passwd':'',
+        'db':'python',
+        'charset':'utf8'
+    }
+    db = DB(db_conf)
+    sql = "select * from test"
+    db.query(sql)
+    result = db.fetchAllRows()
+    for r in result:
+        print r
+
+    db.close()
--- com/nianyu/utils/ftp.py
+++ com/nianyu/utils/ftp.py
@@ -9,167 +9,106 @@ class SSH:
     1、执行命令
     2、拷贝文件
     """
-    _client = None
-    _sftp = None
-    _TIMEOUT = 5
+    _client = None  #ssh客户端
+    _sftp = None  #sftp
+    _TIMEOUT = 5  #连接超时时间
 
-    def __init__(self,ssh_config):
+    def __init__(self,ssh_conf):
 
         try:
             self._client = paramiko.SSHClient()
             self._client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-            self._client.connect(ssh_config['host'],
-                            ssh_config['port'],
-                            ssh_config['user'],
-                            ssh_config['passwd'],
+            self._client.connect(
+                            ssh_conf['host'],
+                            ssh_conf['port'],
+                            ssh_conf['user'],
+                            ssh_conf['passwd'],
                             timeout=self._TIMEOUT)
         except Exception,e:
             print "get client failed: ",e
 
         try:
-            ssh = paramiko.Transport((ssh_config['host'],
-                            ssh_config['port'],))
+            ssh = paramiko.Transport((ssh_conf['host'],
+                            ssh_conf['port'],))
 
-            ssh.connect(username=ssh_config['user'],password=ssh_config['passwd'])
+            ssh.connect(username=ssh_conf['user'],password=ssh_conf['passwd'])
             self._sftp = paramiko.SFTPClient.from_transport(ssh)
 
         except Exception,e:
             print "get sftp failed: ",e
 
 
-    def exc_cmd(self,cmd):
+    def excCmd(self,cmd):
+        """
+        执行命令
+        """
         stdin, stdout, stderr = self._client.exec_command(cmd)
         print stdout.read()
 
 
-    def scp_file(self,file,local_dir,remote_dir):
-
-        self._sftp
-
-
-    def scp_dir(self,local_dir,remote_dir):
+    def scpFile(self,local_file,remote_dir):
         """
-        how to use:
-            ssh = SSH(ssh_config)
-            ssh.scp_dir('/home/niayu','/home/nianyuguai/')
+        上传文件到指定目录
         """
+        remote_file = os.path.join(remote_dir,os.path.split(local_file)[1])
+        try:
+            self._sftp.put(local_file,remote_file)
+            print 'upload %s' % remote_file
+        except Exception,e:
+            print 'upload failed : ',e
+
+
+    def scpDir(self,local_dir,remote_dir):
+        """
+        拷贝目录下的文件到指定目录下
+        """
+
         for parent,dirs,files in os.walk(local_dir):
 
             for file in files:
+
                 local_file = os.path.join(parent,file)
+                path = local_file.replace(local_dir,remote_dir)
 
-                path = local_file.replace(local_dir,'')
                 remote_file = os.path.join(remote_dir,path)
-
                 try:
                     self._sftp.put(local_file,remote_file)
+                    print 'upload %s' % remote_file
                 except Exception,e:
-                    p = os.path.split(remote_file)[0]
-                    self._sftp.mkdir(p)
+                    self._sftp.mkdir(os.path.split(remote_file)[0])
                     self._sftp.put(local_file,remote_file)
+                    print 'upload %s' % remote_file
 
-                for dir in dirs:
-                    local_path = os.path.join(parent,dir)
-                    print local_path
-                    path = local_path.replace(local_dir,'')
-                    print path
-                    remote_path = os.path.join(remote_dir,path)
-                    print remote_path
-                    try:
-                        self._sftp.mkdir(remote_path)
-                        print "mkdir path %s" % remote_path
-                    except Exception,e:
-                        print 'mkdir failed: ',e
-
-def get_client(ip,port,username,passwd):
-    """
-    return :
-        ssh client
-    how to use:
-        client = get_client(ip,22,username,passwd)
-        client.exec_command('ls -alh')
-    """
-    try:
-        client = paramiko.SSHClient()
-        client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        client.connect(ip,port,username,passwd,timeout=5)
-        return client
-    except :
-        print '[Error] : %s ssh connect Error\n'%(ip)
+            for dir in dirs:
+                local_path = os.path.join(parent,dir)
+                path = local_path.replace(local_dir,remote_dir)
+                remote_path = os.path.join(remote_dir,path)
+                try:
+                    cmd = 'mkdir %s' % remote_path.replace("//","/")
+                    self._client.exec_command(cmd)
+                    print cmd
+                except Exception,e:
+                    print '%s failed: ' % cmd
 
 
-def get_sftp(ip,port,username,passwd):
-    """
-    return :
-        sftp object
-    how to use:
-        sftp = sftp(ip,22,username,passwd)
-        sftp.put(localdir,remotedir)
-    """
-
-    try:
-        ssh = paramiko.Transport((ip,port))
-        ssh.connect(username=username,password=passwd)
-        sftp = paramiko.SFTPClient.from_transport(ssh)
-
-        return sftp
-    except:
-        print '[Error] : %s sftp connect Error\n'%(ip)
-
-
-def ssh_cmd(client,cmd):
-    stdin, stdout, stderr = client.exec_command(cmd)
-    for line in stdout.readlines():
-
-        print line
-
-def scp_dir(ip,port,username,passwd,local_dir,remote_dir):
-    """
-    how to use:
-        scp_dir(ip,22,username,passwd,'/home/niayu','/home/nianyuguai/')
-    """
-    sftp = get_sftp(ip,port,username,passwd)
-    client = get_client(ip,port,username,passwd)
-
-    for parent,dirs,files in os.walk(local_dir):
-        for dir in dirs:
-            rdir = os.path.join(parent,dir).replace("\\","/")
-            cmd = 'mkdir %s/%s' %(remote_dir,rdir)
-            print cmd
-            client.exec_command(cmd)
-
-        for file in files:
-            local_file = os.path.join(parent,file)
-            remote_file = remote_dir + '/' + file
-            print 'scp %s %s' % (local_file,remote_file)
-            sftp.put(local_file,remote_file)
-    sftp.close()
-    client.close()
-
-
+    def close(self):
+        """
+        关闭客户端 和 sftp
+        """
+        self._sftp.close()
+        self._client.close()
 
 if __name__=='__main__':
-    # cmd = ['echo hello','ls -al']
-    # username = 'root'
-    # passwd = 'guai'
-    # ip = '192.168.1.152'
-    # sftp = get_sftp(ip,22,username,passwd)
-    # sftp.put('/Users/lixiaojian/Code/python/demo/com/nianyu/utils/ftp.py','/home/nianyuguai/sftp.py')
-    # scp_dir(ip,22,username,passwd,'/Users/lixiaojian/Code/python/demo/com/nianyu/utils/','/home/nianyuguai/Music')
 
-
-    # client = get_client(ip,22,username,passwd)
-    # ssh_cmd(client,'ls -lh')
-    # client.close()
-
-    ssh_config = {
+    ssh_conf = {
         'host':'192.168.1.152',
         'user':'root',
         'passwd':'guai',
         'port':22,
     }
 
-    ssh = SSH(ssh_config)
-    # ssh.exc_cmd('ls -lh')
-
-    ssh.scp_dir('/Users/lixiaojian/Code/python/demo/com','/home/nianyuguai/Music/')
+    ssh = SSH(ssh_conf)
+    # ssh.excCmd('ls -lh')
+    # ssh.scpDir('/Users/lixiaojian/Code/python/demo/com/','/home/nianyuguai/Music/')
+    ssh.scpFile('/Users/lixiaojian/Code/python/demo/com/__init__.py','/home/nianyuguai/')
+    ssh.close()
